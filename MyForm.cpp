b628d8e053a0b76dd7542f0dad8b67d7bd050d93
ArtTeam <ArtemQ98>
2024-07-22 19:37:53 +0300

    'fix'

--- MyForm.cpp
+++ MyForm.cpp
@@ -1,6 +1,8 @@
 #include "MyForm.h"
+#include <typeinfo>
 using namespace System;
 using namespace System::Windows::Forms;
+int intsym[]{0,1,2,3,4,5,6,7,8,9};
 
 [STAThreadAttribute]
 void main(cli::array<String^>^ args)
@@ -9,4 +11,291 @@ void main(cli::array<String^>^ args)
 	Application::EnableVisualStyles();
 	Project2::MyForm form;
 	Application::Run(% form);
+}
+
+inline System::Void Project2::MyForm::btnSearch_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	
+	this->result->Text = "";
+	String^ text1 = this->textBox1->Text;
+	String^ result = this->result->Text;
+	String^ connectionString = "Data Source=ARTTEAM-DESKTOP\\SQLEXPRESS;Initial Catalog=Academy;Integrated Security=True";
+
+	SqlConnection^ connection = gcnew SqlConnection(connectionString);
+	String^ query;
+	if (text1 == "")
+	{
+		query = "SELECT * FROM Students";
+		dataGridView1->Rows->Clear();
+	}
+	else
+	{
+		query = "SELECT * FROM Students WHERE (Name LIKE '%' + '" + text1 + "' + '%' OR CAST(Score	AS varchar(10)) = '" + text1 + "' OR CAST(ID AS varchar(10)) = '" + text1 + "')";
+	}
+	for (int i = 0; i < text1->Length; i++)
+	{
+		if (text1[i] == ' ')
+		{
+			String^ text2 = text1->Substring(i + 1);
+			text1 = text1->Substring(0, i);
+			query = "SELECT * FROM Students WHERE (Name LIKE '%' + '" + text1 + "' + '%' AND Surname LIKE '%' + '" + text2 + "' + '%')";
+			break;
+		}
+	}
+	
+
+
+	SqlCommand^ command = gcnew SqlCommand(query, connection);
+
+
+	SqlDataAdapter^ dataAdapter = gcnew SqlDataAdapter(command);
+	DataSet^ dataSet = gcnew DataSet();
+
+
+	try {
+		connection->Open();
+		dataGridView1->Rows->Clear();
+
+		
+		SqlDataReader^ dbReader = command->ExecuteReader();
+
+		while (dbReader->Read())
+		{
+			dataGridView1->Rows->Add(dbReader["ID"], dbReader["Name"], dbReader["Surname"], dbReader["Score"]);
+		}
+
+		dbReader->Close();
+		connection->Close();
+
+	}
+	catch (SqlException^ ex) {
+		MessageBox::Show(ex->Message);
+	}
+
+
+	/*for (int i = 0; i < size(arr); i++)
+	{
+		if (text1 == "")
+		{
+			this->result->Text = "";
+			break;
+		}
+		for (int j = 0; j < text1->Length; j++)
+		{
+			if (j > size(arr[i]))
+			{
+				continue;
+			}
+			if (tolower(text1[j]) == tolower(arr[i][j]))
+			{
+				c++;
+			}
+		}
+
+		if (c == text1->Length)
+		{
+			c = 0;
+			this->result->Text += gcnew String(arr[i].c_str());
+			this->result->Text += Environment::NewLine;
+		}
+		else
+		{
+			c = 0;
+		}
+	}*/
+}
+
+inline System::Void Project2::MyForm::textBox1_KeyUp(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+	if (e->KeyCode == Keys::Enter)
+	{
+		this->textBox1->Text = "";
+	}
+}
+
+inline System::Void Project2::MyForm::button_Download_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ text1 = this->textBox1->Text;
+	this->textBox1->Text = "";
+	String^ connectionString = "Data Source=ARTTEAM-DESKTOP\\SQLEXPRESS;Initial Catalog=Academy;Integrated Security=True";
+	SqlConnection^ connection = gcnew SqlConnection(connectionString);
+	String^ query;
+	query = "SELECT * FROM Students";
+
+	SqlCommand^ command = gcnew SqlCommand(query, connection);
+	
+
+	SqlDataAdapter^ dataAdapter = gcnew SqlDataAdapter(command);
+	DataSet^ dataSet = gcnew DataSet();
+
+
+	try {
+		connection->Open();
+		dataGridView1->Rows->Clear();
+		SqlDataReader^ dbReader = command->ExecuteReader();
+		//dataAdapter->Fill(dataSet, "Students");
+		//dataGridView1->DataSource = dataSet->Tables["Students"];
+
+		while (dbReader->Read())
+		{
+			dataGridView1->Rows->Add(dbReader["ID"], dbReader["Name"], dbReader["Surname"], dbReader["Score"]);
+		}
+
+		dbReader->Close();
+		connection->Close();
+	}
+	catch (SqlException^ ex) {
+		MessageBox::Show(ex->Message);
+	}
+}
+
+inline System::Void Project2::MyForm::button_Add_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	if (dataGridView1->SelectedRows->Count != 1)
+	{
+		MessageBox::Show("Выберите одну строку для добавления!","Внимание!");
+		return;
+	}
+	int index = dataGridView1->SelectedRows[0]->Index;
+
+	if (dataGridView1->Rows[index]->Cells[0] == nullptr ||
+		dataGridView1->Rows[index]->Cells[1] == nullptr ||
+		dataGridView1->Rows[index]->Cells[2] == nullptr ||
+		dataGridView1->Rows[index]->Cells[3] == nullptr)
+	{
+		MessageBox::Show("Не все данные введены!", "Внимание!");
+		return;
+	}
+	String^ id = dataGridView1->Rows[index-1]->Cells[0]->Value->ToString();
+	int i = Int32::Parse(id) + 1;
+	id = Convert::ToString(i);
+	String^ name = dataGridView1->Rows[index]->Cells[1]->Value->ToString();
+	String^ surname = dataGridView1->Rows[index]->Cells[2]->Value->ToString();
+	String^ score = dataGridView1->Rows[index]->Cells[3]->Value->ToString();
+
+	String^ connectionString = "Data Source=ARTTEAM-DESKTOP\\SQLEXPRESS;Initial Catalog=Academy;Integrated Security=True";
+	SqlConnection^ connection = gcnew SqlConnection(connectionString);
+	String^ query = "INSERT INTO [Students] VALUES ("+ id +", '"+ name +"', '"+ surname +"', "+ score +")";
+	SqlCommand^ command = gcnew SqlCommand(query, connection);
+
+	try {
+		connection->Open();
+		if (command->ExecuteNonQuery() != 1)
+		{
+			MessageBox::Show("Ошибка выполнения запроса", "Ошибка!");
+		}
+		else
+		{
+			MessageBox::Show("Данные были добавлены", "Готово!");
+		}
+
+		connection->Close();
+	}
+	catch (SqlException^ ex) {
+		MessageBox::Show(ex->Message);
+	}
+
+
+}
+
+inline System::Void Project2::MyForm::button_Update_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	if (dataGridView1->SelectedRows->Count != 1)
+	{
+		MessageBox::Show("Выберите одну строку для добавления!", "Внимание!");
+		return;
+	}
+	int index = dataGridView1->SelectedRows[0]->Index;
+
+	if (dataGridView1->Rows[index]->Cells[0] == nullptr ||
+		dataGridView1->Rows[index]->Cells[1] == nullptr ||
+		dataGridView1->Rows[index]->Cells[2] == nullptr ||
+		dataGridView1->Rows[index]->Cells[3] == nullptr)
+	{
+		MessageBox::Show("Не все данные введены!", "Внимание!");
+		return;
+	}
+
+	String^ id = dataGridView1->Rows[index]->Cells[0]->Value->ToString();
+	String^ name = dataGridView1->Rows[index]->Cells[1]->Value->ToString();
+	String^ surname = dataGridView1->Rows[index]->Cells[2]->Value->ToString();
+	String^ score = dataGridView1->Rows[index]->Cells[3]->Value->ToString();
+
+	String^ connectionString = "Data Source=ARTTEAM-DESKTOP\\SQLEXPRESS;Initial Catalog=Academy;Integrated Security=True";
+	SqlConnection^ connection = gcnew SqlConnection(connectionString);
+	String^ query = "UPDATE [Students] SET Name = '"+ name +"', Surname = '"+ surname +"', Score = "+ score +" WHERE ID = "+ id;
+	SqlCommand^ command = gcnew SqlCommand(query, connection);
+
+
+	try {
+		connection->Open();
+		if (command->ExecuteNonQuery() != 1)
+		{
+			MessageBox::Show("Ошибка выполнения запроса", "Ошибка!");
+		}
+		else
+		{
+			MessageBox::Show("Данные были изменены!", "Готово!");
+		}
+
+		connection->Close();
+	}
+	catch (SqlException^ ex) {
+		MessageBox::Show(ex->Message);
+	}
+
+}
+
+inline System::Void Project2::MyForm::button_Delete_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	if (dataGridView1->SelectedRows->Count != 1)
+	{
+		MessageBox::Show("Выберите одну строку для добавления!", "Внимание!");
+		return;
+	}
+	int index = dataGridView1->SelectedRows[0]->Index;
+
+	if (dataGridView1->Rows[index]->Cells[0] == nullptr ||
+		dataGridView1->Rows[index]->Cells[1] == nullptr ||
+		dataGridView1->Rows[index]->Cells[2] == nullptr ||
+		dataGridView1->Rows[index]->Cells[3] == nullptr)
+	{
+		MessageBox::Show("Не все данные введены!", "Внимание!");
+		return;
+	}
+
+	String^ id = dataGridView1->Rows[index]->Cells[0]->Value->ToString();
+	int nextID = Int32::Parse(id)+1;
+	
+	String^ s = Convert::ToString(nextID);
+	
+	String^ connectionString = "Data Source=ARTTEAM-DESKTOP\\SQLEXPRESS;Initial Catalog=Academy;Integrated Security=True";
+	SqlConnection^ connection = gcnew SqlConnection(connectionString);
+
+	String^ query2 = "UPDATE [Students] SET ID = "+ id +" WHERE ID = " + s;
+	SqlCommand^ command2;
+	if (dataGridView1->Rows[index + 1]->Cells[0] != nullptr)
+	{
+		command2 = gcnew SqlCommand(query2, connection);
+	}
+	String^ query = "DELETE FROM [Students] WHERE ID = " + id;
+	SqlCommand^ command = gcnew SqlCommand(query, connection);
+	try {
+		connection->Open();
+		if (command->ExecuteNonQuery() != 1)
+		{
+			MessageBox::Show("Ошибка выполнения запроса", "Ошибка!");
+		}
+		else
+		{
+			MessageBox::Show("Данные были удалены!", "Готово!");
+		}
+		if (command2->ExecuteNonQuery() < 0)
+		{
+			MessageBox::Show("Ошибка выполнения запроса", "Ошибка!");
+		}
+		connection->Close();
+	}
+	catch (SqlException^ ex) {
+		MessageBox::Show(ex->Message);
+	}
+
 }
--- MyForm.h
+++ MyForm.h
@@ -3,9 +3,11 @@
 #include <string>
 
 
+
 using namespace std;
 int c;
-string arr[]{ "Alfa-Romeo", "Aurus", "Audi" };
+string arr[]{ "Alfa-Romeo", "Aurus", "Audi", "Mercedes-Benz", "Changan", "Volvo", "Chevrolet", "BMW", "Chery", "Citroen"};
+
 
 namespace Project2 {
 
@@ -15,7 +17,7 @@ namespace Project2 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-	
+	using namespace System::Data::SqlClient;
 
 	/// <summary>
 	/// Сводка для MyForm
@@ -43,11 +45,38 @@ namespace Project2 {
 			}
 		}
 	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::Button^ btnSearch;
+
 	protected:
 
-	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::TextBox^ result;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^ button_Action;
+
+	private: System::Windows::Forms::ToolStripMenuItem^ button_Download;
+
+	private: System::Windows::Forms::ToolStripMenuItem^ button_Add;
+	private: System::Windows::Forms::ToolStripMenuItem^ button_Update;
+	private: System::Windows::Forms::ToolStripMenuItem^ button_Delete;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dgvID;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dgvName;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dgvSurname;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dgvScore;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	private:
 		/// <summary>
@@ -62,85 +91,218 @@ namespace Project2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::Windows::Forms::DataGridViewCellStyle^ dataGridViewCellStyle1 = (gcnew System::Windows::Forms::DataGridViewCellStyle());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->btnSearch = (gcnew System::Windows::Forms::Button());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->result = (gcnew System::Windows::Forms::TextBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->dgvID = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->dgvName = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->dgvSurname = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->dgvScore = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->button_Action = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->button_Download = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->button_Add = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->button_Update = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->button_Delete = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->menuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(70, 89);
+			this->textBox1->BackColor = System::Drawing::Color::White;
+			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->textBox1->Cursor = System::Windows::Forms::Cursors::IBeam;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->ForeColor = System::Drawing::Color::Black;
+			this->textBox1->Location = System::Drawing::Point(217, 123);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(150, 22);
+			this->textBox1->Size = System::Drawing::Size(260, 22);
 			this->textBox1->TabIndex = 0;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::btnSearch_Click);
+			this->textBox1->KeyUp += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm::textBox1_KeyUp);
 			// 
-			// btnSearch
+			// label1
 			// 
-			this->btnSearch->Location = System::Drawing::Point(106, 127);
-			this->btnSearch->Name = L"btnSearch";
-			this->btnSearch->Size = System::Drawing::Size(75, 23);
-			this->btnSearch->TabIndex = 1;
-			this->btnSearch->Text = L"Serach";
-			this->btnSearch->UseVisualStyleBackColor = true;
-			this->btnSearch->Click += gcnew System::EventHandler(this, &MyForm::btnSearch_Click);
-			// 
-			// textBox2
-			// 
-			this->textBox2->Location = System::Drawing::Point(92, 156);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(0, 22);
-			this->textBox2->TabIndex = 2;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(271, 77);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(161, 29);
+			this->label1->TabIndex = 4;
+			this->label1->Text = L"ARTTEAM";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// result
 			// 
-			this->result->Location = System::Drawing::Point(70, 174);
+			this->result->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(52)), static_cast<System::Int32>(static_cast<System::Byte>(52)),
+				static_cast<System::Int32>(static_cast<System::Byte>(52)));
+			this->result->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->result->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->result->ForeColor = System::Drawing::Color::White;
+			this->result->Location = System::Drawing::Point(217, 337);
 			this->result->Multiline = true;
 			this->result->Name = L"result";
-			this->result->Size = System::Drawing::Size(150, 22);
+			this->result->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->result->Size = System::Drawing::Size(260, 69);
 			this->result->TabIndex = 3;
+			this->result->TextChanged += gcnew System::EventHandler(this, &MyForm::result_TextChanged);
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToOrderColumns = true;
+			this->dataGridView1->BackgroundColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(52)),
+				static_cast<System::Int32>(static_cast<System::Byte>(52)), static_cast<System::Int32>(static_cast<System::Byte>(52)));
+			this->dataGridView1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->dataGridView1->CellBorderStyle = System::Windows::Forms::DataGridViewCellBorderStyle::RaisedVertical;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(4) {
+				this->dgvID,
+					this->dgvName, this->dgvSurname, this->dgvScore
+			});
+			this->dataGridView1->GridColor = System::Drawing::Color::Black;
+			this->dataGridView1->Location = System::Drawing::Point(71, 181);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			dataGridViewCellStyle1->Alignment = System::Windows::Forms::DataGridViewContentAlignment::MiddleLeft;
+			dataGridViewCellStyle1->BackColor = System::Drawing::Color::White;
+			dataGridViewCellStyle1->Font = (gcnew System::Drawing::Font(L"Arial", 7.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			dataGridViewCellStyle1->ForeColor = System::Drawing::Color::Black;
+			dataGridViewCellStyle1->SelectionBackColor = System::Drawing::Color::MediumTurquoise;
+			dataGridViewCellStyle1->SelectionForeColor = System::Drawing::Color::White;
+			dataGridViewCellStyle1->WrapMode = System::Windows::Forms::DataGridViewTriState::True;
+			this->dataGridView1->RowsDefaultCellStyle = dataGridViewCellStyle1;
+			this->dataGridView1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->dataGridView1->Size = System::Drawing::Size(545, 225);
+			this->dataGridView1->TabIndex = 5;
+			// 
+			// dgvID
+			// 
+			this->dgvID->HeaderText = L"ID";
+			this->dgvID->MinimumWidth = 6;
+			this->dgvID->Name = L"dgvID";
+			this->dgvID->Width = 125;
+			// 
+			// dgvName
+			// 
+			this->dgvName->HeaderText = L"Имя";
+			this->dgvName->MinimumWidth = 6;
+			this->dgvName->Name = L"dgvName";
+			this->dgvName->Width = 125;
+			// 
+			// dgvSurname
+			// 
+			this->dgvSurname->HeaderText = L"Фамилия";
+			this->dgvSurname->MinimumWidth = 6;
+			this->dgvSurname->Name = L"dgvSurname";
+			this->dgvSurname->Width = 125;
+			// 
+			// dgvScore
+			// 
+			this->dgvScore->HeaderText = L"Очки";
+			this->dgvScore->MinimumWidth = 6;
+			this->dgvScore->Name = L"dgvScore";
+			this->dgvScore->Width = 125;
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(52)), static_cast<System::Int32>(static_cast<System::Byte>(52)),
+				static_cast<System::Int32>(static_cast<System::Byte>(52)));
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->button_Action });
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(702, 28);
+			this->menuStrip1->TabIndex = 6;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// button_Action
+			// 
+			this->button_Action->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(52)), static_cast<System::Int32>(static_cast<System::Byte>(52)),
+				static_cast<System::Int32>(static_cast<System::Byte>(52)));
+			this->button_Action->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Text;
+			this->button_Action->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(4) {
+				this->button_Download,
+					this->button_Add, this->button_Update, this->button_Delete
+			});
+			this->button_Action->Font = (gcnew System::Drawing::Font(L"Yu Gothic UI Semibold", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button_Action->ForeColor = System::Drawing::Color::Coral;
+			this->button_Action->Name = L"button_Action";
+			this->button_Action->Size = System::Drawing::Size(89, 24);
+			this->button_Action->Text = L"Действия";
+			// 
+			// button_Download
+			// 
+			this->button_Download->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->button_Download->Name = L"button_Download";
+			this->button_Download->Size = System::Drawing::Size(161, 26);
+			this->button_Download->Text = L"Загрузить";
+			this->button_Download->Click += gcnew System::EventHandler(this, &MyForm::button_Download_Click);
+			// 
+			// button_Add
+			// 
+			this->button_Add->Name = L"button_Add";
+			this->button_Add->Size = System::Drawing::Size(161, 26);
+			this->button_Add->Text = L"Добавить";
+			this->button_Add->Click += gcnew System::EventHandler(this, &MyForm::button_Add_Click);
+			// 
+			// button_Update
+			// 
+			this->button_Update->Name = L"button_Update";
+			this->button_Update->Size = System::Drawing::Size(161, 26);
+			this->button_Update->Text = L"Обновить";
+			this->button_Update->Click += gcnew System::EventHandler(this, &MyForm::button_Update_Click);
+			// 
+			// button_Delete
+			// 
+			this->button_Delete->Name = L"button_Delete";
+			this->button_Delete->Size = System::Drawing::Size(161, 26);
+			this->button_Delete->Text = L"Удалить";
+			this->button_Delete->Click += gcnew System::EventHandler(this, &MyForm::button_Delete_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::ControlDarkDark;
-			this->ClientSize = System::Drawing::Size(282, 253);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(52)), static_cast<System::Int32>(static_cast<System::Byte>(52)),
+				static_cast<System::Int32>(static_cast<System::Byte>(52)));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(702, 433);
+			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->result);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->btnSearch);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"SearchALG";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void btnSearch_Click(System::Object^ sender, System::EventArgs^ e) {
-		String^ text1 = this->textBox1->Text;
-		for (int i = 0; i < size(arr); i++)
-		{
-			for (int j = 0; j < text1->Length; j++)
-			{
-				if (text1[j] == arr[i][j])
-				{
-					c++;
-				}
-			}
-
-			if (c == text1->Length)
-			{
-				c = 0;
-				this->result->Text += gcnew String(arr[i].c_str());
-				this->result->Text += "\t";
-			}
-			else
-			{
-				c = 0;
-			}
-		}
-	}
+	private: System::Void btnSearch_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void textBox1_KeyUp(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e);
+private: System::Void result_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button_Download_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void button_Add_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void button_Update_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void button_Delete_Click(System::Object^ sender, System::EventArgs^ e);
 };
 }
